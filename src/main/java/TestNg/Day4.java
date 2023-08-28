c801763a01c7e6cdedb3fc2ce04e513b3deaed66
thoratanushka <thoratanushka12@gmail.com>
2023-08-28 14:08:53 +0530

    new class added day4 and chnage testng.xml file

---
+++ src/main/java/TestNg/Day4.java
@@ -0,0 +1,24 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+	@Test
+	public void carloan() {
+		System.out.println("welcome to car loan");
+	}
+
+	@Test
+	public void webLoginCarLoan() {
+		System.out.println(" in selenium");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		System.out.println(" welcome to appium");
+	}
+	@Test
+	public void APIRestLogin() {
+		System.out.println("api login");
+	}
+}
