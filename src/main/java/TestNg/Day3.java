f3d0fc72f0eab6fc6f4483355cb5bbd6db59d7f1
thoratanushka <thoratanushka12@gmail.com>
2023-08-29 19:38:04 +0530

    parameterized suit level and class level

--- src/main/java/TestNg/Day3.java
+++ src/main/java/TestNg/Day3.java
@@ -1,11 +1,16 @@
 package TestNg;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+
+
 public class Day3 {
+	@Parameters({"URL"})
 	@Test
-	public void HomeLoan() {
+	public void HomeLoan(String urlname) {
 		System.out.println(" welcome to home loan");
+		System.out.println(urlname);
 	}
 
 	@Test
--- src/main/java/TestNg/Day4.java
+++ src/main/java/TestNg/Day4.java
@@ -1,19 +1,20 @@
 package TestNg;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Day4 {
-	@Test
+	@Test(groups ="smoke")
 	public void carloan() {
 		System.out.println("welcome to car loan");
 	}
 
-	@Test
+	@Test(timeOut =4000)
 	public void webLoginCarLoan() {
 		System.out.println(" in selenium");
 	}
 
-	@Test
+	@Test(groups="smoke")
 	public void MobileLoginCarLoan() {
 		System.out.println(" welcome to appium");
 	}
@@ -22,25 +23,26 @@ public class Day4 {
 	public void MobilesignIn() {
 		System.out.println(" welcome to appium1");
 	}
-	@Test
+	@Test(dependsOnMethods ={"MobilesignIn"})
 	public void MobileSignout() {
 		System.out.println(" welcome to appium2");
 	}
 
 
-	@Test
+	@Test(enabled =false)
 	public void MobileLogin() {
 		System.out.println(" welcome to appium1");
 	}
 
-	@Test
+	@Test(enabled=false)
 	public void MobileLogout() {
 		System.out.println(" welcome to appium");
 	}
 
-
+    @Parameters({"URL"})
 	@Test
-	public void APIRestLogin() {
+	public void APIRestLogin(String namevalue) {
 		System.out.println("api login");
+		System.out.println(namevalue);
 	}
 }
